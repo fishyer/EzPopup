ca886644adf7d5dc8699e65dd0cd252fb60aba94
yutianran <yutianran@ezbuy.com>
2019-01-17 12:01:16 +0800

    配置好了nexus maven

--- app/src/main/java/com/ezbuy/ezpopup/MainActivity.java
+++ app/src/main/java/com/ezbuy/ezpopup/MainActivity.java
@@ -3,11 +3,14 @@ package com.ezbuy.ezpopup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ezbuy.library.EzPopup;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new EzPopup().init();
     }
 }
